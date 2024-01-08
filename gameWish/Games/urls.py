71aa95141f380ab0550a67d370f34eb3ffab1072
JuanmisDev <lopezjuanmiguel37@gmail.com>
2024-01-08 14:41:49 -0500

    Pagina info de juegos

--- gameWish/Games/urls.py
+++ gameWish/Games/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.fetch_game_data, name='getData'),
+    path('search/', views.fetch_games_data, name='getData'),
+    path('search/<int:id>', views.game_details, name='game_detail')
 ]
--- gameWish/Games/views.py
+++ gameWish/Games/views.py
@@ -8,7 +8,7 @@ import requests
 
 # Create your views here.
 
-def fetch_game_data(request):
+def fetch_games_data(request):
     
     if request.method == 'GET':
 
@@ -17,7 +17,6 @@ def fetch_game_data(request):
         
         # Define the IGDB API endpoint
         api_url_game = 'https://api.igdb.com/v4/games'
-        api_url_cover = 'https://api.igdb.com/v4/covers'
         query = request.GET.get('query','')
 
         # Define the parameters for the API request
@@ -34,19 +33,54 @@ def fetch_game_data(request):
         }
 
         # Make the API request
-        response_game = requests.post(api_url_game, headers=headers, params=params_game)
+        response_games = requests.post(api_url_game, headers=headers, params=params_game)
         # response_cover = requests.post(api_url_cover, headers=headers, params=params_cover)
 
         # Check if the request was successful (status code 200)
-        if response_game.status_code == 200:
+        if response_games.status_code == 200:
             # Parse the JSON response
-            data_game = response_game.json()
-            return render(request, 'success.html', {'data': data_game,})
+            data_games = response_games.json()
+            return render(request, 'success.html', {'data': data_games,})
         else:
             # Display an error message if the request was unsuccessful
-            return render(request, 'error.html', {'message': f'Error obteniendo datos: {response_game.status_code}'})
+            return render(request, 'error.html', {'message': f'Error obteniendo datos: {response_games.status_code}'})
         
     else:
         return render(request, 'error.html', {'message': 'Método no permitido'})
 
-
+def game_details(request, id):
+    if request.method == 'GET':
+            
+            api_token = 'rllg6x3mwy2nlhuo9dgppr0tkeoqe3'
+            client_id = 'oh1a4v2eabzyogqacl9a5vynyhrai6'
+            
+            # Define the IGDB API endpoint
+            api_url_game = 'https://api.igdb.com/v4/games'
+            # id = request.GET.get('id','')
+    
+            # Define the parameters for the API request
+            params_game = {
+                'fields': f'id, name, cover.image_id, summary; where id = {id}',  # Specify the fields you want to retrieve
+            }
+    
+            # Set up headers with your API key
+            headers = {
+                'Client-ID': client_id,
+                'Authorization': f'Bearer {api_token}',
+            }
+    
+            # Make the API request
+            response_game = requests.post(api_url_game, headers=headers, params=params_game)
+            # response_cover = requests.post(api_url_cover, headers=headers, params=params_cover)
+    
+            # Check if the request was successful (status code 200)
+            if response_game.status_code == 200:
+                # Parse the JSON response
+                data_game = response_game.json()
+                return render(request, 'game_detail.html', {'game': data_game,})
+            else:
+                # Display an error message if the request was unsuccessful
+                return render(request, 'error.html', {'message': f'Error obteniendo datos: {response_game.status_code}'})
+            
+    else:
+        return render(request, 'error.html', {'message': 'Método no permitido'})
